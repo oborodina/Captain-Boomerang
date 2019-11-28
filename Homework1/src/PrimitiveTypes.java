9145951bfdbf3612ffbe3594c140ae5733fdb2fd
oborodina <olgab706@gmail.com>
2019-11-28 21:47:43 +0200

    HW1 Intro

---
+++ Homework1/src/PrimitiveTypes.java
@@ -0,0 +1,18 @@
+/*Try all arithmetic operations (+ - * /) with primitives: byte, short, long, double, float, write results
+of each operation into console (System.out.println())*/
+
+public class PrimitiveTypes {
+    public static void main(String[] args) {
+        byte b = 127;
+        short s = 32767;
+        int i = 2147473647;
+        long l = 1000000l;
+        double d = 32.123456789012345d;
+        float f = 49.123456f ;
+        System.out.println(b+s);
+        System.out.println(l-i);
+        System.out.println(d*b);
+        System.out.println(f/i);
+
+    }
+}
