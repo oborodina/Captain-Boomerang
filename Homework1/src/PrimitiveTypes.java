20fcbb39c270f0f8610c1d52744a836b9ce9686e
oborodina <olgab706@gmail.com>
2019-11-29 20:03:20 +0200

    HW1 code reformation

--- Homework1/src/PrimitiveTypes.java
+++ Homework1/src/PrimitiveTypes.java
@@ -6,13 +6,13 @@ public class PrimitiveTypes {
         byte b = 127;
         short s = 32767;
         int i = 2147473647;
-        long l = 1000000l;
+        long l = 1000000L;
         double d = 32.123456789012345d;
-        float f = 49.123456f ;
-        System.out.println(b+s);
-        System.out.println(l-i);
-        System.out.println(d*b);
-        System.out.println(f/i);
+        float f = 49.123456f;
+        System.out.println(b + s);
+        System.out.println(l - i);
+        System.out.println(d * b);
+        System.out.println(f / i);
 
     }
 }
